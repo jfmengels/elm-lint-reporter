d96473e424273d88f8240728d0586c1324576138
Jeroen Engels <jfm.engels@gmail.com>
2019-08-03 21:18:39 +0200

    Separate tests for better visibility

--- tests/ReporterTest.elm
+++ tests/ReporterTest.elm
@@ -44,57 +44,69 @@ expect { withoutColors, withColors } =
 suite : Test
 suite =
     describe "formatReport"
-        [ test "report that all is fine when there are no errors"
-            (\() ->
-                [ ( { path = "src/FileA.elm"
-                    , source = """module FileA exposing (a)
+        [ noErrorTest
+        , singleErrorTest
+        , multipleErrorsTests
+        ]
+
+
+noErrorTest : Test
+noErrorTest =
+    test "report that all is fine when there are no errors"
+        (\() ->
+            [ ( { path = "src/FileA.elm"
+                , source = """module FileA exposing (a)
 a = Debug.log "debug" 1"""
-                    }
-                  , []
-                  )
-                , ( { path = "src/FileB.elm"
-                    , source = """module FileB exposing (a)
+                }
+              , []
+              )
+            , ( { path = "src/FileB.elm"
+                , source = """module FileB exposing (a)
 a = Debug.log "debug" 1"""
+                }
+              , []
+              )
+            ]
+                |> Reporter.formatReport
+                |> expect
+                    { withoutColors = "I found no linting errors.\nYou're all good!"
+                    , withColors = "I found no linting errors.\nYou're all good!"
                     }
-                  , []
-                  )
-                ]
-                    |> Reporter.formatReport
-                    |> expect
-                        { withoutColors = "I found no linting errors.\nYou're all good!"
-                        , withColors = "I found no linting errors.\nYou're all good!"
+        )
+
+
+singleErrorTest : Test
+singleErrorTest =
+    test "report a single error in a file"
+        (\() ->
+            [ ( { path = "src/FileA.elm"
+                , source = """module FileA exposing (a)
+a = Debug.log "debug" 1"""
+                }
+              , [ { moduleName = Just "FileA"
+                  , ruleName = "NoDebug"
+                  , message = "Do not use Debug"
+                  , details =
+                        [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus erat ullamcorper, commodo leo quis, sollicitudin eros. Sed semper mattis ex, vitae dignissim lectus. Integer eu risus augue. Nam egestas lacus non lacus molestie mattis. Phasellus magna dui, ultrices eu massa nec, interdum tincidunt eros. Aenean rutrum a purus nec cursus. Integer ullamcorper leo non lectus dictum, in vulputate justo vulputate. Donec ullamcorper finibus quam sed dictum."
+                        , "Donec sed ligula ac mi pretium mattis et in nisi. Nulla nec ex hendrerit, sollicitudin eros at, mattis tortor. Ut lacinia ornare lectus in vestibulum. Nam congue ultricies dolor, in venenatis nulla sagittis nec. In ac leo sit amet diam iaculis ornare eu non odio. Proin sed orci et urna tincidunt tincidunt quis a lacus. Donec euismod odio nulla, sit amet iaculis lorem interdum sollicitudin. Vivamus bibendum quam urna, in tristique lacus iaculis id. In tempor lectus ipsum, vehicula bibendum magna pretium vitae. Cras ullamcorper rutrum nunc non sollicitudin. Curabitur tempus eleifend nunc, sed ornare nisl tincidunt vel. Maecenas eu nisl ligula."
+                        ]
+                  , range =
+                        { start = { row = 2, column = 5 }
+                        , end = { row = 2, column = 10 }
                         }
-            )
-        , test "report a single error in a file"
-            (\() ->
-                [ ( { path = "src/FileA.elm"
-                    , source = """module FileA exposing (a)
-a = Debug.log "debug" 1"""
-                    }
-                  , [ { moduleName = Just "FileA"
-                      , ruleName = "NoDebug"
-                      , message = "Do not use Debug"
-                      , details =
-                            [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus erat ullamcorper, commodo leo quis, sollicitudin eros. Sed semper mattis ex, vitae dignissim lectus. Integer eu risus augue. Nam egestas lacus non lacus molestie mattis. Phasellus magna dui, ultrices eu massa nec, interdum tincidunt eros. Aenean rutrum a purus nec cursus. Integer ullamcorper leo non lectus dictum, in vulputate justo vulputate. Donec ullamcorper finibus quam sed dictum."
-                            , "Donec sed ligula ac mi pretium mattis et in nisi. Nulla nec ex hendrerit, sollicitudin eros at, mattis tortor. Ut lacinia ornare lectus in vestibulum. Nam congue ultricies dolor, in venenatis nulla sagittis nec. In ac leo sit amet diam iaculis ornare eu non odio. Proin sed orci et urna tincidunt tincidunt quis a lacus. Donec euismod odio nulla, sit amet iaculis lorem interdum sollicitudin. Vivamus bibendum quam urna, in tristique lacus iaculis id. In tempor lectus ipsum, vehicula bibendum magna pretium vitae. Cras ullamcorper rutrum nunc non sollicitudin. Curabitur tempus eleifend nunc, sed ornare nisl tincidunt vel. Maecenas eu nisl ligula."
-                            ]
-                      , range =
-                            { start = { row = 2, column = 5 }
-                            , end = { row = 2, column = 10 }
-                            }
-                      }
-                    ]
-                  )
-                , ( { path = "src/FileB.elm"
-                    , source = """module FileB exposing (a)
-a = Debug.log "debug" 1"""
-                    }
-                  , []
-                  )
+                  }
                 ]
-                    |> Reporter.formatReport
-                    |> expect
-                        { withoutColors = """-- ELM-LINT ERROR -------------------------------------------------------- FileA
+              )
+            , ( { path = "src/FileB.elm"
+                , source = """module FileB exposing (a)
+a = Debug.log "debug" 1"""
+                }
+              , []
+              )
+            ]
+                |> Reporter.formatReport
+                |> expect
+                    { withoutColors = """-- ELM-LINT ERROR -------------------------------------------------------- FileA
 
 NoDebug: Do not use Debug
 
@@ -107,7 +119,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus erat 
 
 Donec sed ligula ac mi pretium mattis et in nisi. Nulla nec ex hendrerit, sollicitudin eros at, mattis tortor. Ut lacinia ornare lectus in vestibulum. Nam congue ultricies dolor, in venenatis nulla sagittis nec. In ac leo sit amet diam iaculis ornare eu non odio. Proin sed orci et urna tincidunt tincidunt quis a lacus. Donec euismod odio nulla, sit amet iaculis lorem interdum sollicitudin. Vivamus bibendum quam urna, in tristique lacus iaculis id. In tempor lectus ipsum, vehicula bibendum magna pretium vitae. Cras ullamcorper rutrum nunc non sollicitudin. Curabitur tempus eleifend nunc, sed ornare nisl tincidunt vel. Maecenas eu nisl ligula.
 """
-                        , withColors = """[-- ELM-LINT ERROR -------------------------------------------------------- FileA](51-51-51)
+                    , withColors = """[-- ELM-LINT ERROR -------------------------------------------------------- FileA](51-51-51)
 
 [NoDebug](255-255-255): Do not use Debug
 
@@ -120,9 +132,14 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus erat 
 
 Donec sed ligula ac mi pretium mattis et in nisi. Nulla nec ex hendrerit, sollicitudin eros at, mattis tortor. Ut lacinia ornare lectus in vestibulum. Nam congue ultricies dolor, in venenatis nulla sagittis nec. In ac leo sit amet diam iaculis ornare eu non odio. Proin sed orci et urna tincidunt tincidunt quis a lacus. Donec euismod odio nulla, sit amet iaculis lorem interdum sollicitudin. Vivamus bibendum quam urna, in tristique lacus iaculis id. In tempor lectus ipsum, vehicula bibendum magna pretium vitae. Cras ullamcorper rutrum nunc non sollicitudin. Curabitur tempus eleifend nunc, sed ornare nisl tincidunt vel. Maecenas eu nisl ligula.
 """
-                        }
-            )
-        , test "report multiple errors in a file"
+                    }
+        )
+
+
+multipleErrorsTests : Test
+multipleErrorsTests =
+    describe "multiple errors"
+        [ test "report multiple errors in a file"
             (\() ->
                 let
                     details : List String
